6c388d9b0afe6b8d7d041b55788dea3207f726b5
Kasparas915 <146200308+Kasparas915@users.noreply.github.com>
2024-01-10 17:50:48 +0200

    kasparas

---
+++ Assets/scrypts/BallController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D ballRb;
+    public float minVelocity;
+    public float maxVelocity;
+    public float minY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent < Rigidbody2D >();
+
+        ballRb.velocity = new Vector2(0, -minVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(ballRb.velocity.magnitude > maxVelocity)
+        {
+            ballRb.velocity = Vector2.ClampMagnitude(ballRb.velocity, maxVelocity);
+        }
+
+      if(ballRb.velocity.magnitude < minVelocity)
+        {
+            if(ballRb.velocity.y < 0)
+            {
+                ballRb.velocity = new Vector2(ballRb.velocity.x, -minVelocity);
+            }
+
+            if (ballRb.velocity.y > 0)
+            {
+                ballRb.velocity = new Vector2(ballRb.velocity.x, minVelocity);
+            }
+        }
+
+      if(transform.position.y < minY)
+        {
+            transform.position = Vector2.zero;
+            ballRb.velocity = new Vector2(0, -minVelocity);
+        }
+    }
+}
